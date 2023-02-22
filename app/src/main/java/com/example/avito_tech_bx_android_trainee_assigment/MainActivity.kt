ad7c249f4d3f18cc10570c49970221b66c0abec3
milkfist <milkfist@student.21-school.ru>
2023-02-22 18:45:28 +0300

    first commit

---
+++ app/src/main/java/com/example/avito_tech_bx_android_trainee_assigment/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.avito_tech_bx_android_trainee_assigment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
