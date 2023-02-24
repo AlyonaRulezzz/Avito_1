495e0a3f6ae837d39538181280bfe57819343f28
milkfist <milkfist@student.21-school.ru>
2023-02-24 20:44:58 +0300

    Юрины добавления

--- app/src/main/java/com/example/avito_tech_bx_android_trainee_assigment/MainActivity.kt
+++ app/src/main/java/com/example/avito_tech_bx_android_trainee_assigment/MainActivity.kt
@@ -2,16 +2,19 @@ package com.example.avito_tech_bx_android_trainee_assigment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.avito_tech_bx_android_trainee_assigment.adapter.NumberAdapter
 import com.example.avito_tech_bx_android_trainee_assigment.databinding.ActivityMainBinding
 import com.example.avito_tech_bx_android_trainee_assigment.model.NumberModel
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
     lateinit var adapter: NumberAdapter
     lateinit var recyclerView: RecyclerView
+    var list = myNumber()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,12 +25,29 @@ class MainActivity : AppCompatActivity() {
 
     private fun initial() {
         recyclerView = binding.rvNumber
-        adapter = NumberAdapter()
+        adapter = NumberAdapter(::adapterListener)
         recyclerView.adapter = adapter
-        adapter.setList(myNumber())
+
+        adapter.setList(list)
+//        deleteItem(14)
     }
 
-    fun myNumber(): ArrayList<NumberModel>{
+    private fun adapterListener(value: Int) {
+        Toast.makeText(this, "deleted $value", Toast.LENGTH_LONG).show()
+        adapter.setList(list.filter { it.number != value })
+    }
+
+    private fun deleteItem(i: Int) {
+        GlobalScope.launch {
+            delay(2_000)
+            withContext(Dispatchers.Main) {
+                adapter.setList(List(i) {NumberModel(it + 1, "x")})
+            }
+            deleteItem(i - 1)
+        }
+    }
+
+    private fun myNumber(): ArrayList<NumberModel>{
         val numberList = ArrayList<NumberModel>()
 
         lateinit var number: NumberModel
@@ -36,6 +56,8 @@ class MainActivity : AppCompatActivity() {
             number = NumberModel(i, "x")
             numberList.add(number)
         }
+
+
 
         return numberList
     }
--- app/src/main/java/com/example/avito_tech_bx_android_trainee_assigment/adapter/NumberAdapter.kt
+++ app/src/main/java/com/example/avito_tech_bx_android_trainee_assigment/adapter/NumberAdapter.kt
@@ -1,34 +1,53 @@
 package com.example.avito_tech_bx_android_trainee_assigment.adapter
 
 import android.annotation.SuppressLint
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.avito_tech_bx_android_trainee_assigment.R
+import com.example.avito_tech_bx_android_trainee_assigment.databinding.ItemNumberLayoutBinding
 import com.example.avito_tech_bx_android_trainee_assigment.model.NumberModel
 import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.item_number_layout.view.*
+import kotlin.properties.Delegates
 
-class NumberAdapter: RecyclerView.Adapter<NumberAdapter.NumberViewHolder>() {
+class NumberAdapter(
+    val listener: (value: Int) -> Unit,
+): RecyclerView.Adapter<NumberAdapter.NumberViewHolder>() {
+
 
     private var numberList = emptyList<NumberModel>()
 
-    class NumberViewHolder(view: View): RecyclerView.ViewHolder(view)
+    private var i = 0
+
+    inner class NumberViewHolder(val binding: ItemNumberLayoutBinding): RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(value: NumberModel) {
+            itemView.tv_number.text = value.number.toString()
+            itemView.tv_delete.text = value.x
+            binding.ivCancel.setBackgroundResource(R.drawable.ic_launcher_background)
+            itemView.tv_delete.setOnClickListener {
+                listener(value.number)
+//            holder.itemView.rv_number.removeViewAt(position)
+                notifyItemRemoved(position)
+//            notifyDataSetChanged()
+            }
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumberViewHolder {
-        var view = LayoutInflater.from(parent.context).inflate(R.layout.item_number_layout, parent, false)
-        return NumberViewHolder(view)
+        Log.d("MY_LOG", "onCreateViewHolder: ${++i}")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_number_layout, parent, false)
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemNumberLayoutBinding.inflate(inflater, parent, false)
+        return NumberViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: NumberViewHolder, position: Int) {
-        holder.itemView.tv_number.text = numberList[position].number.toString()
-        holder.itemView.tv_delete.text = numberList[position].x
-        holder.itemView.tv_delete.setOnClickListener {
-            holder.itemView.rv_number.removeViewAt(position)
-            notifyItemRemoved(position)
-//            notifyDataSetChanged()
-        }
+        val item = numberList[position]
+        holder.bind(item)
     }
 
     override fun getItemCount(): Int {
