d395738ada264ae0efd2cbb3794512ce293e665d
milkfist <milkfist@student.21-school.ru>
2023-08-02 20:14:47 +0300

    recyclerView inside viewModel moved to Fragment and works

--- app/src/main/java/viewmodel/ViewmodelRecyclerviewFragment.kt
+++ app/src/main/java/viewmodel/ViewmodelRecyclerviewFragment.kt
@@ -1,5 +1,6 @@
 package viewmodel
 
+import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.content.Context
 import android.content.SharedPreferences
@@ -12,22 +13,74 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.avito_tech_bx_android_trainee_assigment.R
-import com.example.avito_tech_bx_android_trainee_assigment.adapter.NumberAdapter
+//import com.example.avito_tech_bx_android_trainee_assigment.adapter.NumberAdapter
 import com.example.avito_tech_bx_android_trainee_assigment.databinding.FragmentViewmodelRecyclerviewBinding
+import com.example.avito_tech_bx_android_trainee_assigment.databinding.ItemNumberLayoutBinding
 import com.example.avito_tech_bx_android_trainee_assigment.model.NumberModel
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import kotlinx.android.synthetic.main.item_number_layout.view.*
 
 
 class ViewmodelRecyclerviewFragment : Fragment() {
 
+    class NumberAdapter(
+        val listener: (value: Int) -> Unit,
+    ): RecyclerView.Adapter<NumberAdapter.NumberViewHolder>() {
+
+
+        private var numberList = emptyList<NumberModel>()
+
+        private var i = 0
+
+        inner class NumberViewHolder(val binding: ItemNumberLayoutBinding): RecyclerView.ViewHolder(binding.root) {
+
+            private val context = binding.root.context
+
+            fun bind(value: NumberModel) {
+                itemView.tv_number.text = value.number.toString()
+                val res = ContextCompat.getDrawable(context, R.drawable.ic_baseline_delete_24)
+                itemView.iv_cancel.setImageDrawable(res)
+                itemView.iv_cancel.setOnClickListener {
+                    listener(value.number)
+                    notifyItemRemoved(layoutPosition)
+                }
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumberViewHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.item_number_layout, parent, false)
+            val inflater = LayoutInflater.from(parent.context)
+            val binding = ItemNumberLayoutBinding.inflate(inflater, parent, false)
+            return NumberViewHolder(binding)
+        }
+
+        override fun onBindViewHolder(holder: NumberViewHolder, position: Int) {
+            val item = numberList[position]
+            holder.bind(item)
+        }
+
+        override fun getItemCount(): Int {
+            return numberList.size
+        }
+
+        @SuppressLint("NotifyDataSetChanged")
+        fun setList(list: List<NumberModel>) {
+            numberList = list
+            notifyDataSetChanged()
+        }
+    }
+////////////////////////////////////////
     private val adapter: NumberAdapter = NumberAdapter(::adapterListener)
 
-    private val binding: FragmentViewmodelRecyclerviewBinding by lazy {
-        FragmentViewmodelRecyclerviewBinding.inflate(layoutInflater)
-    }
+//    private val binding: FragmentViewmodelRecyclerviewBinding by lazy {
+//        FragmentViewmodelRecyclerviewBinding.inflate(layoutInflater)
+//    }
+    lateinit var binding: FragmentViewmodelRecyclerviewBinding
 
     private lateinit var factory: ViewmodelRecyclerviewViewModelFactory
 //    var factory = ViewmodelRecyclerviewViewModelFactory(loadListFromSharedPreferences(requireContext(), "listOfNumberModel"))
@@ -51,11 +104,26 @@ class ViewmodelRecyclerviewFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        var factory = ViewmodelRecyclerviewViewModelFactory(loadListFromSharedPreferences(requireContext(), "listOfNumberModel"))
+        factory = ViewmodelRecyclerviewViewModelFactory(loadListFromSharedPreferences(requireContext(), "listOfNumberModel"))
 //    private val viewModel by lazy {
         viewModel = ViewModelProvider(this, factory).get(ViewmodelRecyclerviewViewModel::class.java)
 //    }
-        return inflater.inflate(R.layout.fragment_viewmodel_recyclerview, container, false)
+
+        // Сначала раздуть макет и получить переменную binding
+        val rootView = inflater.inflate(R.layout.fragment_viewmodel_recyclerview, container, false)
+
+        // После получения binding, установите адаптер для RecyclerView
+//        binding = FragmentViewmodelRecyclerviewBinding.inflate(layoutInflater)
+        binding = FragmentViewmodelRecyclerviewBinding.bind(rootView)
+        binding.rvNumber.adapter = adapter
+        binding.rvNumber.layoutManager = if (resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            GridLayoutManager(context, 2)
+        } else {
+            GridLayoutManager(context, 4)
+        }
+        initViewModel()
+
+        return rootView
     }
 
     @Deprecated("Deprecated in Java")
@@ -64,13 +132,6 @@ class ViewmodelRecyclerviewFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
 //        super.onCreate(savedInstanceState)
 //        setContentView(binding.root)
-        binding.rvNumber.adapter = adapter
-        binding.rvNumber.layoutManager = if (resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
-            GridLayoutManager(context, 2)
-        } else {
-            GridLayoutManager(context, 4)
-        }
-        initViewModel()
     }
 
     private fun initViewModel() {
